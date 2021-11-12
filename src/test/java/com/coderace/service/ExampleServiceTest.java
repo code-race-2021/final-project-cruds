8b014cc20514b7f84339b4816d241cd2bdb6e4df
Ortola <nicolas.ortola@mercadolibre.com>
2021-11-12 10:07:03 -0300

    test and contracts

--- src/test/java/com/coderace/service/ExampleServiceTest.java
+++ src/test/java/com/coderace/service/ExampleServiceTest.java
@@ -4,6 +4,7 @@ import com.coderace.model.dtos.ExampleRequestDTO;
 import com.coderace.model.dtos.ExampleResponseDTO;
 import com.coderace.model.entities.Example;
 import com.coderace.model.enums.ExampleEnum;
+import com.coderace.model.exceptions.BadRequestException;
 import com.coderace.repository.ExampleRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,6 +12,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
 
@@ -42,6 +44,76 @@ class ExampleServiceTest {
         final ExampleResponseDTO responseDTO = service.create(requestDTO);
 
         assertEquals(service.buildExampleResponseDTO(example), responseDTO);
+    }
+
+    @Test
+    @DisplayName("create | given invalid long value | should throw BadRequestException")
+    void createInvalidLongValueOk() {
+        final ExampleRequestDTO requestDTO = new ExampleRequestDTO().setLongValue(-5L);
+
+        final BadRequestException exception = assertThrows(BadRequestException.class, () -> service.create(requestDTO));
+
+        assertAll("Expected exception",
+                () -> assertEquals(HttpStatus.BAD_REQUEST.value(), exception.getStatusCode()),
+                () -> assertEquals("longValue must be greater than 0", exception.getMessage())
+        );
+    }
+
+    @Test
+    @DisplayName("create | given invalid date value | should throw BadRequestException")
+    void createInvalidDateValueOk() {
+        final String invalidDateString = "not-a-date";
+
+        final ExampleRequestDTO requestDTO = new ExampleRequestDTO()
+                .setLongValue(1L)
+                .setDateValue(invalidDateString);
+
+        final BadRequestException exception = assertThrows(BadRequestException.class, () -> service.create(requestDTO));
+
+        assertAll("Expected exception",
+                () -> assertEquals(HttpStatus.BAD_REQUEST.value(), exception.getStatusCode()),
+                () -> assertEquals("Invalid dateValue: " + invalidDateString, exception.getMessage())
+        );
+    }
+
+    @Test
+    @DisplayName("create | given invalid enum value | should throw BadRequestException")
+    void createInvalidEnumValueOk() {
+        final String invalidEnumString = "c";
+
+        final ExampleRequestDTO requestDTO = new ExampleRequestDTO()
+                .setLongValue(5L)
+                .setDateValue(LocalDateTime.MAX.toString())
+                .setEnumValue(invalidEnumString);
+
+        final BadRequestException exception = assertThrows(BadRequestException.class, () -> service.create(requestDTO));
+
+        assertAll("Expected exception",
+                () -> assertEquals(HttpStatus.BAD_REQUEST.value(), exception.getStatusCode()),
+                () -> assertEquals("Invalid enumValue: " + invalidEnumString, exception.getMessage())
+        );
+    }
+
+    @Test
+    @DisplayName("buildExampleResponseDTO | ok")
+    void buildExampleResponseDTO() {
+        final long longValue = 1L;
+        final double doubleValue = 2D;
+        final String stringValue = "a";
+        final LocalDateTime dateValue = LocalDateTime.MIN;
+        final ExampleEnum enumValue = ExampleEnum.B;
+
+        final Example example = new Example(longValue, doubleValue, stringValue, dateValue, enumValue);
+
+        final ExampleResponseDTO dto = service.buildExampleResponseDTO(example);
+
+        assertAll("Expected dto",
+                () -> assertEquals(longValue, dto.getLongValue()),
+                () -> assertEquals(doubleValue, dto.getDoubleValue()),
+                () -> assertEquals(stringValue, dto.getStringValue()),
+                () -> assertEquals(dateValue.toString(), dto.getDateValue()),
+                () -> assertEquals(enumValue.getCode(), dto.getEnumValue())
+        );
     }
 
 }
