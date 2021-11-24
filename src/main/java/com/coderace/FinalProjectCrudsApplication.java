145e361e594af329056b9ce9af1035ff99795443
Ortola <nicolas.ortola@mercadolibre.com>
2021-11-24 20:26:48 -0300

    PROFIN-1 comento Main.java

--- src/main/java/com/coderace/FinalProjectCrudsApplication.java
+++ src/main/java/com/coderace/FinalProjectCrudsApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FinalProjectCrudsApplication {
 
+	// Este es el punto de entrada del sistema
 	public static void main(String[] args) {
 		SpringApplication.run(FinalProjectCrudsApplication.class, args);
 	}
