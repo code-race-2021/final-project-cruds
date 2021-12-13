e488d18b01572d2a72df907099b2451fdbb673a8
Ortola <nicolas.ortola@mercadolibre.com>
2021-12-13 12:42:51 -0300

    elimino otro espacio

--- src/main/java/com/coderace/FinalProjectCrudsApplication.java
+++ src/main/java/com/coderace/FinalProjectCrudsApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FinalProjectCrudsApplication {
-
 	// Este es el punto de entrada del sistema
 	public static void main(String[] args) {
 		SpringApplication.run(FinalProjectCrudsApplication.class, args);
